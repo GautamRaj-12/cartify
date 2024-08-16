032a7d5832a6865fa471f2903db4da4143c8bd14
Gautam Raj <gautamraj996@gmail.com>
2024-08-16 15:00:25 +0530

    Style cart page

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -7,6 +7,7 @@ import {
   updateItemQuantity,
   removeItem,
 } from "@/lib/features/cart/cartSlice";
+import { FaTrash } from "react-icons/fa6";
 
 const Cart = () => {
   const cartItems = useSelector((state: RootState) => state.cart.items);
@@ -27,42 +28,61 @@ const Cart = () => {
     <>
       <Header />
       <section className="w-1/2 mx-auto my-6">
+        <p className="text-xl font-bold mt-6 text-right mb-6">
+          Total: ${cartTotal.toFixed(2)}
+        </p>
+        <div className="grid grid-cols-3 gap-8 mb-4 items-center">
+          <div className="bg-[#101014]">
+            <p className="text-center text-white p-2 rounded-sm">Item</p>
+          </div>
+          <div className="bg-[#101014]">
+            <p className="text-center text-white p-2 rounded-sm">
+              Price(per unit)
+            </p>
+          </div>
+          <div className="bg-[#101014]">
+            <p className="text-center text-white p-2 rounded-sm">Qty</p>
+          </div>
+        </div>
         {cartItems.map((cartItem) => {
           return (
             <div
               key={cartItem.id}
-              className="grid grid-cols-5 gap-8 mb-4 items-center"
+              className="grid grid-cols-3 gap-8 mb-4 items-center"
             >
-              <div className="justify-self-end">
+              <div className="self-center justify-self-start flex justify-center items-center gap-6 p-1">
                 <img
                   src={cartItem.image}
                   alt={cartItem.title}
                   className="w-20 h-20"
                 />
+                <h1 className="justify-self-start">
+                  {cartItem.title.slice(0, 30) + "..."}
+                </h1>
               </div>
-              <h1 className="content-center">
-                {cartItem.title.slice(0, 30) + "..."}
-              </h1>
-              <h2 className="content-center">${cartItem.price.toFixed(2)}</h2>
-              <input
-                type="number"
-                value={cartItem.quantity}
-                min="1"
-                onChange={(e) =>
-                  handleQuantityChange(cartItem.id, parseInt(e.target.value))
-                }
-                className="w-16 text-center border border-gray-300 rounded"
-              />
-              <button
-                onClick={() => handleRemoveItemClick(cartItem.id)}
-                className="text-red-600 hover:underline"
-              >
-                Remove Item
-              </button>
+              <h2 className="justify-self-center">
+                ${cartItem.price.toFixed(2)}
+              </h2>
+              <div className="justify-self-center flex gap-6">
+                <input
+                  type="text"
+                  value={cartItem.quantity}
+                  min="1"
+                  onChange={(e) =>
+                    handleQuantityChange(cartItem.id, parseInt(e.target.value))
+                  }
+                  className="w-16 text-center border border-gray-300 rounded"
+                />
+                <button
+                  onClick={() => handleRemoveItemClick(cartItem.id)}
+                  className="text-red-600 hover:underline"
+                >
+                  <FaTrash />
+                </button>
+              </div>
             </div>
           );
         })}
-        <p className="text-xl font-bold mt-6">Total: ${cartTotal.toFixed(2)}</p>
       </section>
     </>
   );
