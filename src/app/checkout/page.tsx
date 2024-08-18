c26778a3d15d6bdb6b7627312b1932cfc45d791c
Gautam Raj <gautamraj996@gmail.com>
2024-08-18 11:29:25 +0530

    Make checkout page responsive

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -6,15 +6,15 @@ const Checkout = () => {
     <>
       <div className="flex flex-col w-screen h-screen items-center justify-center gap-6 text-textPrimary">
         <div className="flex items-center justify-center gap-6">
-          <h2 className="text-[48px]">Order has been placed</h2>
-          <FaHeart color="#FB2E86" fontSize={48} />
+          <h2 className="sm:text-[48px] text-3xl">Order has been placed</h2>
+          <FaHeart color="#FB2E86" className="sm:text-[48px] text-3xl" />
         </div>
         <h3 className="text-3xl">Thank You</h3>
         <Link
           href="/"
           className="italic font-semibold border-b-2 border-accent"
         >
-          Back to Home
+          Continue Shopping
         </Link>
       </div>
     </>
